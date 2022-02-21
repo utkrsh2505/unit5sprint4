71f57e0b30810ed68db5a24fb074079e8a7c01cb
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-02-21 21:56:59 +0530

    update

--- src/ContextApi/AuthContext.jsx
+++ src/ContextApi/AuthContext.jsx
@@ -4,6 +4,10 @@ export const Auth = createContext();
 const AuthContext = ({children})=>{
     const [isLogin,setIsLogin] = useState(false);
     const handleLogin = (name,password)=>{
+        if(name=="" || password==""){
+            alert("please fill details");
+            return;
+        }
         if( name === "admin" && password=== "admin"){
             setIsLogin(true);
 
